58500f45ec3ea7b6ac590715e73384fc199d4ae9
Tatiane Patriota <tatianepatriota1@gmail.com>
2022-06-23 14:40:04 -0300

    Projeto Finalizado

---
+++ client/js/app/helpers/DateHelper.js
@@ -0,0 +1,27 @@
+class DateHelper {
+    constructor(){
+        throw new Error('Está classe não pode ser intanciada')
+    }
+
+    static dataParaTexto(data){
+        return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+
+    }
+    
+    static textoParaData(texto){
+
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+            throw new Error('A data deve estar no formato ano/mês/dia');
+        };
+
+        return new Date (...texto.split('-').map((item, indice) => {
+            if(indice === 1){
+                return item -1;
+            }else {
+                return item;
+            }
+        }));
+    }
+
+
+}
